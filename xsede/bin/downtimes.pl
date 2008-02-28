28fa34ad3b381428308096f6c100a4593ce8d0cf
Kate Ericson <kericson@sdsc.edu>
2008-02-28 21:21:18 +0000

    add emailing, remove pw (changed oracle pw)
    git-svn-id: file:///localdisk/subversion/inca/trunk/devel/projects/teragrid@9552 7dba3f4a-8be6-0310-8b3b-b4fec25ea7f3

--- xsede/bin/downtimes.pl
+++ xsede/bin/downtimes.pl
@@ -7,19 +7,23 @@ use DateTime;
 use DateTime::Format::Strptime;
 
 my $dir = "/misc/inca/install-2r5/webapps/xsl/";
-my $cacheFile = $dir . "downtimes.properties";
-my $tmpFile = $dir . "downtimes.properties.tmp";
+my $cacheFile = $dir . "downtime.properties";
+my $tmpFile = $dir . "downtime.properties.tmp";
+my $pw = `cat $ENV{'HOME'}/bin/downtimes.db`;
+$pw =~ s/\n//g;
 
-my $service = q{inca/inca_test@(DESCRIPTION =
+my $service = "inca/". $pw . "@(DESCRIPTION =
           (ADDRESS = (PROTOCOL = TCP)(HOST = spike-vip.sdsc.edu)(PORT = 1521))
           (ADDRESS = (PROTOCOL = TCP)(HOST = jet-vip.sdsc.edu)(PORT = 1521))
           (ADDRESS = (PROTOCOL = TCP)(HOST = faye-vip.sdsc.edu)(PORT = 1521))
           (LOAD_BALANCE = on)(FAILOVER = on)(CONNECT_DATA =
           (SID = NPACI_RAC)(SERVER = DEDICATED)(SERVICE_NAME = npaci.sdsc.edu)
-          (FAILOVER_MODE = (TYPE = select)(METHOD = basic))))};
+          (FAILOVER_MODE = (TYPE = select)(METHOD = basic))))";
 
 my $dbh = DBI->connect('dbi:Oracle:', $service, '') || die "Database connect err: $DBI::errstr";
 my $now = DateTime->now;
+#debug time that will get a down resource
+#$now = DateTime::Format::Strptime->new(pattern=>'%Y-%m-%d %l.%M.%S %p')->parse_datetime('2008-02-26 08.00.00 AM')->set_time_zone('America/Chicago'); 
 
 my $query = "SELECT i.item_id, i.subject, i.content, 
                 s.event_start_time, + s.event_end_time, s.event_time_zone, 
@@ -42,18 +46,22 @@ if ( !defined $sth ) {
   die "Cannot prepare statement: $DBI::errstr\n";
 }
 $sth->execute();
+my $email = "";
 open TMP,">$tmpFile";
 while ( my ($id, $subject, $content, $start, $end, $zone, $name ) = $sth->fetchrow()){
   my $startDate = convertToDateTime($start, $zone);
   my $endDate = convertToDateTime($end, $zone);
   if ($startDate <= $now && $endDate >= $now){
     print TMP "$name=$id\n";
+    $email .= "$name, http://news.teragrid.org/view-item.php?item=$id\n";
   }
 }
 $dbh->disconnect();
 close TMP;
 `mv $tmpFile $cacheFile`;
-
+if ($email ne ""){
+  `echo "$email" | mail -s "TeraGrid News DB has resources down" inca\@sdsc.edu`
+}
 
 sub convertToDateTime{
   my $date = shift;
