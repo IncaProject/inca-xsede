b4ce847b5599222b2696458bd9219b465430a68e
Shava Smallen <ssmallen@sdsc.edu>
2012-05-16 23:19:15 +0000

    - due to a timezone issue, am checking future downtimes as well as current
    - due to a change in the news database, we are now getting resource ids in the
      form of <resourcename>-<site>.  
    
    
    git-svn-id: file:///localdisk/subversion/inca/trunk/devel/projects/teragrid@14768 7dba3f4a-8be6-0310-8b3b-b4fec25ea7f3

--- xsede/bin/downtimes.pl
+++ xsede/bin/downtimes.pl
@@ -7,36 +7,57 @@ use LWP::Simple;
 use XML::XPath;
 use XML::XPath::XMLParser;
 use Data::Dumper;
+use Date::Manip;
 
 my $config = `cat /localdisk/inca/teragrid/var/resources.xml`;
 my $xp = XML::XPath->new( xml => $config );
 
 # get current downtimes
-my $outfile = get("http://info.teragrid.org/web-apps/csv/tg-outages-v1/current/");
-if ($?){
-  print "could not get outage file\n$!";
-} else {
-  my @outages = split(/\n/, $outfile);
-  my $pre = "/localdisk/inca/teragrid/webapps";
-  my $prop = "$pre/../etc/downtime.properties";
-  my $iis =  "$pre/inca/html/downtimes-iis.txt";
-  open PROP,">$prop";
-  open IIS,">$iis";
-  for my $i (1 .. $#outages){
-    my @line = split(/,/, $outages[$i]);
-    my $iis_resource = $line[1];
-    my ($news_id) = $line[2] =~ /(\d+)"/;
-    print IIS "$iis_resource=$news_id\n";
-    my $rnode = $xp->find("/res:resourceConfig/resources/resource[name='$iis_resource']/macros/macro[name='__regexp__']/value/text()");
-    foreach my $node ($rnode->get_nodelist) {
-      my $resource = XML::XPath::XMLParser::as_string($node);
-      my @resources = split(/ /, $resource);
-      foreach my $r (@resources) {
-        print PROP "$r=$news_id\n";
+# future is looked at too since there were some timezone parsing problems
+my @urls = ( "http://info.teragrid.org/web-apps/csv/tg-outages-v1/future/", "http://info.teragrid.org/web-apps/csv/tg-outages-v1/current/");
+my $pre = "/localdisk/inca/teragrid/webapps";
+my $prop = "$pre/../etc/downtime.properties";
+my $iis =  "$pre/inca/html/downtimes-iis.txt";
+open PROP,">$prop";
+open IIS,">$iis";
+for my $url ( @urls ) {
+  my $outfile = `wget -q -O - $url`;
+  if ($? || ! defined $outfile ){
+    print "could not get outage file\n";
+  } else {
+    my @outages = split(/\n/, $outfile);
+    for my $i (1 .. $#outages){
+      my @line = split(/,/, $outages[$i]);
+      # 5/16/12 -- resource ids got reversed to <resource-name>-<site>
+      my $iis_resource = $line[1];
+      my @iis_resource_ids = split( /-/, $iis_resource );
+      next if scalar(@iis_resource_ids) < 2; # not a resource-site pair
+      my $site = $iis_resource_ids[$#iis_resource_ids];
+      next if $site eq "news";
+      my $resource = $iis_resource_ids[0];
+      my $start= $line[$#line-1];
+      $start =~ s/"//g;
+      my $end= $line[$#line];
+      $end =~ s/"//g;
+      my $startDate = ParseDate($start);
+      my $endDate = ParseDate($end);
+      my $now = ParseDate("now");
+      if ( Date_Cmp($now, $startDate) >= 0 &&
+           (! defined $endDate || $endDate eq "" || Date_Cmp($now, $endDate)< 1) ) {
+        my ($news_id) = $line[2] =~ /(\d+)"/;
+        my $rnode = $xp->find("/res:resourceConfig/resources/resource[name='$resource-$site']/macros/macro[name='__regexp__']/value/text()");
+        print PROP "$site-$resource=$news_id\n";
+        foreach my $node ($rnode->get_nodelist) {
+          my $resource = XML::XPath::XMLParser::as_string($node);
+          my @resources = split(/ /, $resource);
+          foreach my $r (@resources) {
+            print IIS "$r=$news_id\n";
+          }
+        }
       }
     }
   }
-  close PROP;
-  close IIS;
-  `cp $prop $pre/inca/html/downtimes.txt`;
 }
+close PROP;
+close IIS;
+`cp $prop $pre/inca/html/downtimes.txt`;
