6feeba834fe44853f662590ad1c3e58c81d9363d
Shava Smallen <ssmallen@sdsc.edu>
2014-01-08 15:57:59 +0000

    ignore incommon certs
    git-svn-id: file:///localdisk/subversion/inca/trunk/devel/projects/teragrid@15364 7dba3f4a-8be6-0310-8b3b-b4fec25ea7f3

--- xsede/bin/downloadCerts.pl
+++ xsede/bin/downloadCerts.pl
@@ -6,17 +6,15 @@ use warnings;
 my $dir = "/etc/grid-security/certificates.$$";
 mkdir( $dir ) or die "Unable to create temp directory";
 chdir( $dir ) or die "Unable to cd to temp directory";
-`wget --no-check-certificate
-https://software.xsede.org/security/xsede-certs.tar.gz 2>&1`;
+`wget --no-check-certificate https://software.xsede.org/security/xsede-certs.tar.gz 2>&1`;
 `tar zxvf xsede-certs.tar.gz`;
-my $out = `diff -x '*.r0' -r certificates /etc/grid-security/certificates`;
+my $out = `diff -x '*.r0' --exclude-from=/etc/grid-security/exclude.txt -r certificates /etc/grid-security/certificates`;
 if ( $? != 0 ) {
   `rm -fr /etc/grid-security/certificates.old`;
   `mv /etc/grid-security/certificates /etc/grid-security/certificates.old`;
   `mv $dir/certificates /etc/grid-security/certificates`;
-   `cp /etc/grid-security/InCommon-Server-CA /etc/grid-security/certificates`
-   open( MAIL, '| mail -s "quarry certs were refreshed" ssmallen@sdsc.edu') ||
-die "Unable to mail";
+  `cp /etc/grid-security/InCommon-Server-CA/* /etc/grid-security/certificates`;
+   open( MAIL, '| mail -s "quarry certs were refreshed" ssmallen@sdsc.edu') || die "Unable to mail";
    print MAIL $out;
    close MAIL;
 } 
