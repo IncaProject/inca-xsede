35ab1713ed37751e70e2ed0ee3950a8c2fe0f126
inca <inca@sdsc.edu>
2016-12-01 21:01:13 +0000

    UPdate to new ifo services
    git-svn-id: file:///localdisk/subversion/inca/trunk/devel/projects/teragrid@16075 7dba3f4a-8be6-0310-8b3b-b4fec25ea7f3

--- xsede/bin/downtimes.pl
+++ xsede/bin/downtimes.pl
@@ -3,6 +3,7 @@
 
 use strict;
 use warnings;
+use JSON;
 
 my $INCA_INSTALL;
 BEGIN { # force variable to be set during compile
@@ -20,37 +21,38 @@ my $xp = XML::XPath->new( xml => $config );
 
 # get current downtimes
 # future is looked at too since there were some timezone parsing problems
-my @urls = ( "http://info.teragrid.org/web-apps/csv/tg-outages-v1/future/", "http://info.teragrid.org/web-apps/csv/tg-outages-v1/current/");
+#my @urls = ( "https://info1.dyn.xsede.org:443/wh1/outages/v1/outages/Current", "https://info1.dyn.xsede.org:443/wh1/outages/v1/outages/Future/");
+my @urls = ( "https://info1.dyn.xsede.org:443/wh1/outages/v1/outages");
 my $pre = "$INCA_INSTALL/webapps";
 my $prop = "$INCA_INSTALL/etc/downtime.properties";
 my $iis =  "$pre/inca/html/downtimes-iis.txt";
 open PROP,">$prop";
 open IIS,">$iis";
 for my $url ( @urls ) {
-  my $outfile = `wget -q -O - $url`;
+  my $outfile = `wget --no-check-certificate -q -O - $url`;
   if ($? || ! defined $outfile ){
     print "could not get outage file\n";
   } else {
-    my @outages = split(/\n/, $outfile);
+    use Data::Dumper;
+    my $json = decode_json($outfile);
+    my @outages = @{$json};
     for my $i (1 .. $#outages){
-      my @line = split(/,/, $outages[$i]);
-      # 10/9/12:  expecting iis id on second field
-      my $iis_resource = $line[1];
+      my $iis_resource = $outages[$i]->{"ResourceID"};
       next if $iis_resource =~ /general-user-news/;
-      my $start= $line[$#line-1];
-      $start =~ s/"//g;
-      my $end= $line[$#line];
-      $end =~ s/"//g;
+      my $start = $outages[$i]->{"OutageStart"};
+      my $end= $outages[$i]->{"OutageEnd"};
       my $startDate = ParseDate($start);
       my $endDate = ParseDate($end);
       my $now = ParseDate("now");
       if ( Date_Cmp($now, $startDate) >= 0 &&
            (! defined $endDate || $endDate eq "" || Date_Cmp($now, $endDate)< 1) ) {
-        my ($news_id) = $line[2] =~ /(\d+)"/;
+        print "Found news item";
+        my $news_id = $outages[$i]->{"OutageID"};
         # 10/9/12 -- lonestar has 2 registrations in IIS lonestar and lonestar4
         my $search_resource = $iis_resource =~ /lonestar/ ? "lonestar4.tacc.teragrid.org" : $iis_resource;
         $search_resource = $search_resource =~ /ranch/ ? "ranch.tacc.xsede.org" : $search_resource;
-        $search_resource = $search_resource =~ /condor/ ? "condor.purdue.teragrid.org" : $search_resource;
+        $search_resource = $search_resource =~ /stampede/ ? "stampede.tacc.xsede.org" : $search_resource;
+        $search_resource =~ s/teragrid/xsede/ if $search_resource =~ /darter|gatech/;
         my $rnode = $xp->find("/res:resourceConfig/resources/resource[name='$search_resource']/macros/macro[name='__regexp__']/value/text()");
         print IIS "$iis_resource=$news_id\n";
         foreach my $node ($rnode->get_nodelist) {
